9beb587db18912c05a7e02c682d6920098d3932a
Roman Lenskij <roman@quadrarubra.com>
2016-04-21 16:03:00 +0300

    Backport rabbit autocluster from master

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer 'openstack-chef'
 maintainer_email 'openstack-dev@lists.openstack.org'
 license 'Apache 2.0'
 description 'Provides the shared messaging configuration for Chef for OpenStack.'
-version '12.0.0'
+version '12.0.2'
 
 recipe 'server', 'Installs and configures server packages for messaging queue used by the deployment.'
 recipe 'rabbitmq-server', 'Installs and configures RabbitMQ and is called via the server recipe'
